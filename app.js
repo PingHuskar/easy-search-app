8473037119678567ce7b6e4a8c8421e4ed2381c2
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-05-13 23:50:41 +0700

    add 2 more web

--- app.js
+++ app.js
@@ -7,13 +7,22 @@ function main() {
         }
     }
     var ids = ["udemy","udemy_mycourse","google","github_all","github_myrepo","youtube",
-    "iqiyi","imdb","applemusic","hareshi","stackoverflow","th.wikipedia","forms","sheets","slides","datastudio","drive","steam"]
+    "iqiyi","imdb","applemusic","hareshi","stackoverflow","th.wikipedia","forms","sheets","slides","datastudio","drive","steam","alternativeto","th1lib"]
     ids.map(opens)
 }
 // const element = document.querySelector('img');
 // element.style.opacity = "0.5";
 // element.style.filter = 'alpha(opacity=90)';
+$("img").mouseenter(function() {
+    $("#log").html(this.getAttribute('id')).css('display','block')
+})
+$("img").mouseleave(function() {
+    $("#log").html('').css({
+        'display':'inherit'
+    })
+})
 $("img").click(function(){
     console.log(this)
+    $("#log").html(this.getAttribute('id') + " clicked !")
     $(this).toggleClass('clicked')
 });
