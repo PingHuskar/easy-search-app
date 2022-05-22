32266fb3c43e35d1aea7d05a2f71420df23f8bc7
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-05-22 14:50:11 +0700

    add sidebar + jQuery

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var log = []
-
+$("img").hide()
+openNav()
 function main() {
     function opens(web) {
-        if (/(round\s)?clicked/.test(document.getElementById(web).getAttribute('class'))) {
+        if (/.*clicked$/.test(document.getElementById(web).getAttribute('class'))) {
             open(document.getElementById(web).getAttribute('url')+document.getElementById('search').value);
         }
     }
     var ids = ["udemy","udemy_mycourse","google","github_all","github_myrepo","youtube",
-    "iqiyi","imdb","applemusic","hareshi","stackoverflow","th.wikipedia","forms","sheets","slides","datastudio","drive","steam","alternativeto","th1lib"]
+    "iqiyi","imdb","applemusic","hareshi","stackoverflow","th.wikipedia","forms","sheets",
+    "slides","datastudio","drive","steam","alternativeto","th1lib","goodreads"]
     ids.map(opens)
 }
 // const element = document.querySelector('img');
@@ -26,3 +28,36 @@ $("img").click(function(){
     $("#log").html(this.getAttribute('id') + " clicked !")
     $(this).toggleClass('clicked')
 });
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+function showall() {
+    $("img").show().removeClass("clicked")
+}
+
+function template() {
+    $("img").hide().removeClass("clicked")
+    $("ppp").show().addClass("clicked")
+}
+function google() {
+    $("img").hide().removeClass("clicked")
+    $(".google").show()
+}
+function movie() {
+    $("img").hide().removeClass("clicked")
+    $(".video").show()
+    $(".movie").show()
+    $(".anime").show().addClass("clicked")
+}
+
+function book() {
+    $("img").hide().removeClass("clicked")
+    $(".book").show().addClass("clicked")
+}
